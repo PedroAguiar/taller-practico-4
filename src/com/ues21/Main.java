9edf040c53dd9c71749c66695a058e7f586ebae7
Pedro Aguiar <pepoaguiar@gmail.com>
2018-07-18 20:47:39 -0300

    changing quicksort implementation

--- src/com/ues21/Main.java
+++ src/com/ues21/Main.java
@@ -1,29 +1,75 @@
 package com.ues21;
 
-import com.sun.xml.internal.bind.v2.model.annotation.Quick;
 import com.ues21.algoritmos.Quicksort;
 import com.ues21.algoritmos.Shellsort;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
+//        testSortingWithOrderedArrays();
+        testSortingWithUnOrderedArrays();
+    }
+
+    private static void testSortingWithUnOrderedArrays() {
+        final int[] tenIntegers = generateRandomArray(10);
         final int[] hundredIntegers = generateRandomArray(100);
         final int[] thousandIntegers = generateRandomArray(1000);
-        final int[] tensThousandIntegers = generateRandomArray(10000);
-        final int[] hundredThousandsIntegers = generateRandomArray(100000);
-        final int[] millionIntegers = generateRandomArray(1000000);
-        final List<int[]> arrays = new ArrayList<>();
-        arrays.add(hundredIntegers);
-        arrays.add(thousandIntegers);
-        arrays.add(tensThousandIntegers);
-        arrays.add(hundredThousandsIntegers);
-        arrays.add(millionIntegers);
-        testShellSort(arrays);
-        testQuickSort(arrays);
+        final int[] tenThousandIntegers = generateRandomArray(10_000);
+        final int[] hundredThousandsIntegers = generateRandomArray(100_000);
+        final int[] millionIntegers = generateRandomArray(1_000_000);
+        final int[] tenMillionIntegers = generateRandomArray(10_000_000);
+        final int[] hundredMillionIntegers = generateRandomArray(100_000_000);
+
+
+        final List<int[]> unorderedArrays = new ArrayList<>();
+        unorderedArrays.add(tenIntegers);
+        unorderedArrays.add(hundredIntegers);
+        unorderedArrays.add(thousandIntegers);
+        unorderedArrays.add(tenThousandIntegers);
+        unorderedArrays.add(hundredThousandsIntegers);
+        unorderedArrays.add(millionIntegers);
+        unorderedArrays.add(tenMillionIntegers);
+        unorderedArrays.add(hundredMillionIntegers);
+
+        testShellSort(unorderedArrays);
+        testQuickSort(unorderedArrays);
+    }
+
+    private static void testSortingWithOrderedArrays() {
+        final int[] orderedTenIntegers = generateRandomArray(10);
+        final int[] orderedHundredIntegers = generateRandomArray(100);
+        final int[] orderedThousandIntegers = generateRandomArray(1000);
+        final int[] orderedTenThousandIntegers = generateRandomArray(10_000);
+        final int[] orderedHundredThousandsIntegers = generateRandomArray(100_000);
+        final int[] orderedMillionIntegers = generateRandomArray(1_000_000);
+        final int[] orderedTenMillionIntegers = generateRandomArray(10_000_000);
+        final int[] orderedHundredMillionIntegers = generateRandomArray(100_000_000);
+
+
+        Arrays.sort(orderedTenIntegers);
+        Arrays.sort(orderedHundredIntegers);
+        Arrays.sort(orderedThousandIntegers);
+        Arrays.sort(orderedTenThousandIntegers);
+        Arrays.sort(orderedHundredThousandsIntegers);
+        Arrays.sort(orderedMillionIntegers);
+        Arrays.sort(orderedTenMillionIntegers);
+        Arrays.sort(orderedHundredMillionIntegers);
+
+
+        final List<int[]> orderedArrays = new ArrayList<>();
+        orderedArrays.add(orderedTenIntegers);
+        orderedArrays.add(orderedHundredIntegers);
+        orderedArrays.add(orderedThousandIntegers);
+        orderedArrays.add(orderedTenThousandIntegers);
+        orderedArrays.add(orderedHundredThousandsIntegers);
+        orderedArrays.add(orderedMillionIntegers);
+        orderedArrays.add(orderedTenMillionIntegers);
+        orderedArrays.add(orderedHundredMillionIntegers);
+
+        testShellSort(orderedArrays);
+        testQuickSort(orderedArrays);
     }
 
     private static void testQuickSort(List<int[]> integersList) {
@@ -34,7 +80,7 @@ public class Main {
 
     private static void executeQuickSort(int[] integers) {
         long startTime = System.currentTimeMillis();
-        Quicksort.quicksort(integers, integers[0], integers[integers.length - 1]);
+        Quicksort.sort(integers);
         long endTime = System.currentTimeMillis();
         long elapsedTime = endTime - startTime;
         System.out.println("Performance using quicksort for " + integers.length + " items is: " + elapsedTime + "ms.");
--- src/com/ues21/algoritmos/Quicksort.java
+++ src/com/ues21/algoritmos/Quicksort.java
@@ -1,56 +1,50 @@
 package com.ues21.algoritmos;
 
 public class Quicksort {
-    private static final int CUTOFF = 10;
+    private static int array[];
+    private static int length;
 
-    public static void quicksort(int[] a, int low, int high) {
-        if(low + CUTOFF > high) {
-            insertionSort(a, low, high);
-        } else {
-            // Sort low, middle, high
-            int middle = (low + high) / 2;
-            if(a[middle] < a[low]) {
-                swapReferences(a, low, middle);
-            }
-            if(a[high] < a[low]) {
-                swapReferences(a, low, high);
-            }
-            if(a[high] < a[middle]) {
-                swapReferences(a, middle, high);
-            }
-            swapReferences(a, middle, high - 1);
-            int pivot = a[high - 1];
-            int i, j;
-            for(i = low, j = high - 1;;) {
-                while(a[++i] <  pivot);
-                while(pivot < a[--j]);
-                if(i < j) {
-                    swapReferences( a, i, j );
-                } else {
-                    break;
-                }
-            }
-            swapReferences(a, i,high - 1);
-            quicksort(a, low,i - 1);
-            quicksort(a,i + 1, high);
+    public static void sort(int[] inputArr) {
+
+        if (inputArr == null || inputArr.length == 0) {
+            return;
         }
+        array = inputArr;
+        length = inputArr.length;
+        quickSort(0, length - 1);
     }
 
-    private static void swapReferences(int[] a, int index1, int index2) {
-        int tmp = a[index1];
-        a[index1] = a[index2];
-        a[index2] = tmp;
+    private static void quickSort(int lowerIndex, int higherIndex) {
+        int i = lowerIndex;
+        int j = higherIndex;
+        // calculate pivot number, I am taking pivot as middle index number
+        int pivot = array[lowerIndex+(higherIndex-lowerIndex)/2];
+        // Divide into two arrays
+        while (i <= j) {
+            while (array[i] < pivot) {
+                i++;
+            }
+            while (array[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                exchangeNumbers(i, j);
+                //move index to next position on both sides
+                i++;
+                j--;
+            }
+        }
+        // call quickSort() method recursively
+        if (lowerIndex < j)
+            quickSort(lowerIndex, j);
+        if (i < higherIndex)
+            quickSort(i, higherIndex);
     }
 
-    private static void insertionSort(int[] a, int low, int high) {
-        for(int p = low + 1; p <= high; p++) {
-            int tmp = a[p];
-            int j;
-            for(j = p; j > low && tmp < a[j - 1]; j--) {
-                a[j] = a[j - 1];
-            }
-            a[j] = tmp;
-        }
+    private static void exchangeNumbers(int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 
 }
